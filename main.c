1a7adaba2a167bcd78ab47f7aa14ad237d55012a
dima <168024863+krovskiy@users.noreply.github.com>
2025-09-23 17:01:30 +0300

    portal for admin and customer added, password checker

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
+// Checks if the user made a valid input
 
 int validNumber(void) {
     int x;
     while (1) {
-        if (scanf("%d", &x)&&(x == 1 || x == 2)) {
+        if (scanf_s("%d", &x)&&(x == 1 || x == 2)) {
             return x;
         }
         else {
@@ -14,25 +16,54 @@ int validNumber(void) {
     }
 }
 
-
 int welcomeDialog(char store[]) {
     printf("Welcome to the %s Store! \n\n"
            "**************************************************\n\n"
-           "Type '1' if you're a USER navigating through the site\n"
-           "or type '2' if you're an ADMINISTRATOR and want some changes!\n\n"
+           "Type '1' if you're a CUSTOMER navigating through the site\n"
+           "or type '2' if you're an STOREKEEPER and want some changes!\n\n"
            "**************************************************\n"
            "Happy shopping! :)\n", store);
     printf("INPUT: ");
     return validNumber();
 }
 
+void customerMenu() {
+    //pasdomsething
+}
 
+void storekeeperMenu() {
+    //ask password first
+}
+
+void storeKeeperPassCheck() {
+    char adminPass[20];
+    const char correctPassword[] = "admin123";
+
+    printf("Insert the admin password to access the panel!\n");
+    // Use scanf for a single-word password
+    scanf("%19s", adminPass);
+
+    if (strcmp(adminPass, correctPassword) == 0) {
+        printf("Correct password, welcome to the admin panel!\n");
+        storekeeperMenu();
+    } else {
+        printf("Wrong password, access denied!\n");
+    }
+}
+
+void roleSelect(int roleChoice) {
+    if (roleChoice == 1) {
+        printf("you are a customer :)\n");
+        customerMenu();
+    } else {
+        printf("you are a storekeeper :)\n");
+        storeKeeperPassCheck();
+    }
+}
 
 int main(void) {
-    char store[] = "SOMETHING";
-    int choice = welcomeDialog(store);
-
+    char store[] = "Frag.ee";
+    int roleChoice = welcomeDialog(store);
+    roleSelect(roleChoice);
     return 0;
-
-
 }
