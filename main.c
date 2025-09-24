115b486b0f836d35c62601988d41f30fb54ffe2b
dima <168024863+krovskiy@users.noreply.github.com>
2025-09-25 02:19:55 +0300

    documentation for release 1 and small changes

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 int validNumber(void) {
     int x;
     while (1) {
-        if (scanf_s("%d", &x) && (x == 1 || x == 2)) {
+        if (scanf("%d", &x) && (x == 1 || x == 2)) {
             return x;
         } else {
-            printf("Not a valid input! Try again.\n");
+            printf("ERROR: Not a valid input! Try again.\n");
             printf("INPUT: ");
             int c;
             while ((c = getchar()) != '\n' && c != EOF) { } //Error handling when an input doesn't work!
@@ -16,6 +16,7 @@ int validNumber(void) {
     }
 }
 
+// Displays the welcome banner and asks for the user role
 int welcomeDialog() {
     printf("___________                                        \n");
     printf("\\_   _____/___________     ____      ____   ____  \n");
@@ -34,56 +35,67 @@ int welcomeDialog() {
     return validNumber();
 }
 
+// Placeholder for customer functionality
 void customerMenu() {
     printf("You are now in the customer section. You can browse and make purchases.\n");
     // You would add the code to display products and handle purchases here
 }
 
+// Storekeeper menu functionality
 void storekeeperMenu() {
     int adminChoice;
-    printf("\n--- Welcome to the Storekeeper Panel! ---\n");
-    printf("What would you like to do?\n");
-    printf("1. Add a new product\n");
-    printf("2. View all products\n");
-    printf("3. Exit to main menu\n");
-    printf("INPUT: ");
-    
-    // Using scanf_s to get the admin's choice
-    scanf_s("%d", &adminChoice);
-    
-    switch(adminChoice) {
-        case 1:
-            printf("You would add a new product here. (Functionality not implemented yet)\n");
-            break;
-        case 2:
-            printf("You would view all products here. (Functionality not implemented yet)\n");
-            break;
-        case 3:
-            printf("Exiting admin panel. Goodbye!\n");
-            break;
-        default:
-            printf("Invalid choice.\n");
-            break;
+
+    while (1) {  // keep looping until the user chooses "3"
+        printf("\n--- Storekeeper Panel ---\n");
+        printf("What would you like to do?\n");
+        printf("1. Add a new product\n");
+        printf("2. View all products\n");
+        printf("3. Exit to main menu\n");
+        printf("INPUT: ");
+
+        if (scanf("%d", &adminChoice) != 1) {
+            // flush bad input
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {} //gets character, returns int, until we get ENTER so we can flush the pipe
+            printf("ERROR: Please enter a number.\n");
+            continue;
+        }
+
+        switch (adminChoice) {
+            case 1:
+                printf(">> Add product functionality coming soon...\n");
+                break;
+            case 2:
+                printf(">> View products functionality coming soon...\n");
+                break;
+            case 3:
+                printf("Exiting admin panel. Goodbye!\n");
+                return;  // break out of the loop and return to caller
+            default:
+                printf("ERROR: Invalid choice. Please try again.\n");
+                break;
+        }
     }
 }
 
+// Password gate for storekeeper access
 void storeKeeperPassCheck() {
     char adminPass[20];
     const char correctPassword[] = "admin123";
 
     printf("Insert the admin password to access the panel!\n");
-    // Use scanf_s for a single-word password
-    scanf_s("%19s", adminPass, sizeof(adminPass));
+    // Use scanf for a single-word password
+    scanf("%19s", adminPass, sizeof(adminPass));
 
     if (strcmp(adminPass, correctPassword) == 0) {
         printf("Correct password, welcome to the admin panel!\n");
         storekeeperMenu();
     } else {
-        printf("Wrong password, access denied!\n");
+        printf("ERROR: Wrong password, access denied!\n");
         storeKeeperPassCheck();
     }
 }
-
+// Routes user based on their chosen role
 void roleSelect(int roleChoice) {
     if (roleChoice == 1) {
         printf("You are a customer :)\n");
@@ -94,6 +106,7 @@ void roleSelect(int roleChoice) {
     }
 }
 
+// Entry point
 int main(void) {
     const int roleChoice = welcomeDialog();
     roleSelect(roleChoice);
