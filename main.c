0112a1d27dcd2449b8b7bc9d2704a72e38898c99
dima <168024863+krovskiy@users.noreply.github.com>
2025-10-21 14:15:24 +0300

    Release 2 beginning

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Need to implement the following:
+ * Functions (there are as of Release 1)
+ * Decision tables
+ * Arrays, Strings, Data Dictionaries
+ * Pointers
+ * Parameter Passing
+*/
+
+//Function for converting rateToEur[]
+
+//Structure for Products & balance codes
+
+typedef struct {
+    char name[50];   // e.g., "Eau de Luxe"
+    char brand[50]; // e.g., "Chanel"
+    float price;           // e.g., 120.50
+    int stock;             // e.g., 15 bottles available
+    float discount; // e.g., how big is the discount
+} Perfume;
+
+typedef struct {
+    char code[6];
+    float value;
+    int used; // 0 = not used, 1 = redeemed
+} BalanceCode;
+
+//ANDREAS & SASHA - ADMIN PANEL - IMPLEMENT addPerfume(), viewPerfume(), setDiscount(), addBalanceCode() work this with Britten! ;
+
+//BRITTEN - USER PANEL - Implement addBalance() - work this with Andreas! , viewProducts();
+
+//DIMA - SRS, ERROR HANDLING, FILE HANDLING LATER, EVERYTHING TO CHECK AT THE END
+
+//HENRIK - FLOWCHARTS, USER STORIES (3 new ones related to the functions that
+// other will implement), Implementation Options for Release 2 (SRS) & Decision Tables (SRS);
+
+//SASHA - Data Dictionary (SRS) & Acceptance Criteria for Release 2 (SRS);
+
+Perfume inventory[100];
+int perfumeCount = 0;
+
+BalanceCode codes[100];
+int codeCount = 0;
+
+float userBalance = 0.0f;
+
 // Checks if the user made a valid input
 int validNumber(void) {
     int x;
@@ -40,44 +86,88 @@ int welcomeDialog() {
 void customerMenu() {
     printf("You are now in the customer section. You can browse and make purchases.\n");
     // You would add the code to display products and handle purchases here
+    int userChoice;
+    do {
+        printf("\n--- Customer Panel ---\n");
+        printf("1. Add a new product\n");
+        printf("2. View all products\n");
+        printf("3. View shopping cart\n");
+        printf("4. Exit to main menu\n");
+        printf("INPUT: ");
+
+        if (scanf("%d", &userChoice) != 1) {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            printf("ERROR: Please enter a number.\n");
+            continue;
+        }
+
+        switch (userChoice) {
+            case 1:
+                // addProducts();
+                break;
+            case 2:
+                // viewProducts();
+                break;
+            case 3:
+                // viewShoppingCart();
+                break;
+            case 4:
+                printf("Exiting customer panel. Goodbye!\n");
+                break;
+            default:
+                printf("ERROR: Invalid choice. Please try again.\n");
+                break;
+        }
+        // Exit after handling options 1,2,3
+        if (userChoice >= 1 && userChoice <= 3) {
+            break;  // exit the menu loop after performing the action
+        }
+    } while (userChoice != 4);
 }
 
 // Storekeeper menu functionality
 void storekeeperMenu() {
     int adminChoice;
 
-    while (1) {  // keep looping until the user chooses "3"
-        printf("\n--- Storekeeper Panel ---\n");
-        printf("What would you like to do?\n");
-        printf("1. Add a new product\n");
-        printf("2. View all products\n");
-        printf("3. Exit to main menu\n");
-        printf("INPUT: ");
+    printf("\n--- Storekeeper Panel ---\n");
+    printf("What would you like to do?\n");
+    printf("1. Add a new product\n");
+    printf("2. View all products\n");
+    printf("3. Set discount\n");
+    printf("4. Add balance code\n");
+    printf("5. Exit to main menu\n");
+    printf("INPUT: ");
 
-        if (scanf("%d", &adminChoice) != 1) {
-            // flush bad input
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF) {} //gets character, returns int, until we get ENTER so we can flush the pipe
-            printf("ERROR: Please enter a number.\n");
-            continue;
-        }
+    if (scanf("%d", &adminChoice) != 1) {
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {} // flush bad input
+        printf("ERROR: Please enter a number.\n");
+        return;
+    }
 
-        switch (adminChoice) {
-            case 1:
-                printf(">> Add product functionality coming soon...\n");
-                break;
-            case 2:
-                printf(">> View products functionality coming soon...\n");
-                break;
-            case 3:
-                printf("Exiting admin panel. Goodbye!\n");
-                return;  // break out of the loop and return to caller
-            default:
-                printf("ERROR: Invalid choice. Please try again.\n");
-                break;
-        }
+    switch (adminChoice) {
+        case 1:
+            // addPerfume();
+            break;
+        case 2:
+            // viewPerfume();
+            break;
+        case 3:
+            // setDiscount();
+            break;
+        case 4:
+            // addBalanceCode();
+            break;
+        case 5:
+            printf("Exiting admin panel. Goodbye!\n");
+            return;
+        default:
+            printf("ERROR: Invalid choice. Please try again.\n");
+            break;
     }
 }
+
 
 // Password gate for storekeeper access
 void storeKeeperPassCheck() {
